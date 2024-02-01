f5c907dd4e8a07b1a2edef4a2d29316264a211d3
kitcurio <kasiarivers@gmail.com>
2024-02-01 08:28:04 -0500

    Version 1 - Working on daily reset. Onboarding view started but not fleshed out.

---
+++ Widget/WidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  WidgetBundle.swift
+//  Widget
+//
+//  Created by Kasia Rivers on 2/1/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct WidgetyBundle: WidgetBundle {
+    var body: some Widget {
+        Widgety()
+    }
+}
